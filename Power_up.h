7e162550cb3f1c57ecdb8c1d2481d00d80a6197f
Cameron Morrow <cmorrow2@csub.edu>
2016-04-26 19:13:36 -0700

    Added files via upload

---
+++ Power_up.h
@@ -0,0 +1,15 @@
+struct PowerUp {
+	//type is going to be an int ie(1:"Shield Bubble" 2:"Unlimited Ammo" 3:"Walk Through Walls" etc...)
+	int type;
+	int lifeSpan;
+	//position[0] = X; position[1] = Y;
+	int position[2];
+
+	void operator=(int input){
+		type = input;
+		switch(input){
+			//fill in info about lifespan, name, sprite, and other info here upon initialization
+		}
+	}
+};
+
