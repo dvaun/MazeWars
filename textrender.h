a15c3088a746be652cc5a78155a67857a3d75fb8
David Vaughan <david@Davids-MacBook-Pro.local>
2016-05-07 03:13:01 -0700

    setting up blocks to generate

--- textrender.h
+++ textrender.h
@@ -2,6 +2,6 @@
 #define TRENDER_H
 
 void drawBlackNum(float x, float y, float offsetx, float offsety, float printvalue);
-void drawWhiteNum(float x, float y, float offsetx, float offsety, float printvalue)
+void drawWhiteNum(float x, float y, float offsetx, float offsety, float printvalue);
 
 #endif
