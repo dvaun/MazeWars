14f3da25e57d314bf6b6d3def414fd11cb2da44b
David Vaughan <david@Davids-MacBook-Pro.local>
2016-05-06 23:48:24 -0700

    testing textur changes

--- jobG.cpp
+++ jobG.cpp
@@ -71,20 +71,12 @@ GLuint silhouetteTexture)
 	
 	glBindTexture(GL_TEXTURE_2D, personTexture);
 	//
-	glTexStorage2D(GL_TEXTURE_2D, num_mipmaps, GL_RGBA8, width, height);
-	glTexSubImage2D(GL_TEXTURE_2D, 0​, 0, 0, width​, height​, GL_BGRA, GL_UNSIGNED_BYTE, pixels);
-	glGenerateMipmap(GL_TEXTURE_2D);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexImage2D(GL_TEXTURE_2D, 0, 3, w, h, 0, GL_RGB, 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, w, h, 0, GL_BGRA, 
 			GL_UNSIGNED_BYTE, personImage->data);
 //
 	//
-  //Generate num_mipmaps number of mipmaps here.
 
 	//
 	glBindTexture(GL_TEXTURE_2D, silhouetteTexture);
