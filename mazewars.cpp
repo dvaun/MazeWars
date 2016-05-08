ff819a5a5f84e6b8e90ea26a7197923f6167b69b
David Vaughan <david@Davids-MacBook-Pro.local>
2016-05-07 18:44:47 -0700

    weird issue with gun gpos

--- mazewars.cpp
+++ mazewars.cpp
@@ -358,52 +358,29 @@ void check_mouse(XEvent *e, Game *g)
 
 void pointPlayer(Game *g, int savex, int savey)
 {
-//Make the player point at the cursor
+	//Make the player point at the cursor
 	g->gun.pos[0] = g->Player_1.gpos[0];
 	g->gun.pos[1] = g->Player_1.gpos[1];
 	float weaponx = g->gun.pos[0];
 	float weapony = g->gun.pos[1];
 
 	float nDeg = atan(((yres-savey)-(weapony))/\
-		((savex)-(weaponx))) * 180 / PI;
+					((savex)-(weaponx))) * 180 / PI;
 
 	if (savex > weaponx && (yres - savey) > weapony)
-		nDeg += 180;
+			nDeg += 180;
 	if (savex > weaponx && (yres - savey) < weapony)
-		nDeg -= 180;
+			nDeg -= 180;
 
 	if (g->gun.angle > 360.f)
-		g->gun.angle = 360.0f;
+			g->gun.angle = 360.0f;
 	if (g->gun.angle <= 360.0f) {
-		if (nDeg > 270)
-			nDeg -= 360;
-		g->gun.angle = nDeg + 90;
+			if (nDeg > 270)
+					nDeg -= 360;
+			g->gun.angle = nDeg + 90;
 	}
 	if (g->gun.angle < 0.0f)
-		g->gun.angle += 360.0f;
-		//Make the player point at the cursor
-		g->gun.pos[0] = g->Player_1.pos[0];
-		g->gun.pos[1] = g->Player_1.pos[1];
-		float weaponx = g->gun.pos[0];
-		float weapony = g->gun.pos[1];
-
-		float nDeg = atan(((yres-savey)-(weapony))/\
-						((savex)-(weaponx))) * 180 / PI;
-
-		if (savex > weaponx && (yres - savey) > weapony)
-				nDeg += 180;
-		if (savex > weaponx && (yres - savey) < weapony)
-				nDeg -= 180;
-
-		if (g->gun.angle > 360.f)
-				g->gun.angle = 360.0f;
-		if (g->gun.angle <= 360.0f) {
-				if (nDeg > 270)
-						nDeg -= 360;
-				g->gun.angle = nDeg + 90;
-		}
-		if (g->gun.angle < 0.0f)
-				g->gun.angle += 360.0f;
+			g->gun.angle += 360.0f;
 }
 
 /*void getJoystickEvent(JoystickEvent event)
