312b4052f258e5f001d361d4e984b2ef63d954b7
David Vaughan <david@Davids-MacBook-Pro.local>
2016-05-07 23:51:00 -0700

    testing pikachu.png load

--- jobG.cpp
+++ jobG.cpp
@@ -85,9 +85,7 @@ GLuint silhouetteTexture)
 	GLuint tex_2d = SOIL_load_OGL_texture("images/pikachu.png",
 					SOIL_LOAD_AUTO,
 					SOIL_CREATE_NEW_ID,
-					SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y |
-					 SOIL_FLAG_NTSC_SAFE_RGB |
-					  SOIL_FLAG_COMPRESS_TO_DXT);
+					SOIL_FLAG_NTSC_SAFE_RGB);
 
 }
 
