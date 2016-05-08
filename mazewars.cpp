87d4833b3d70d797b22ad653d2dca336ac71cdbe
David Vaughan <david@Davids-MacBook-Pro.local>
2016-05-07 20:20:15 -0700

    forgot to change color initialization for player

--- mazewars.cpp
+++ mazewars.cpp
@@ -80,65 +80,6 @@ void timeCopy(struct timespec *dest, struct timespec *source)
 //-----------------------------------------------------------------------------
 
 int xres=1250, yres=900;
-
-/*struct Bullet {
-  Vec pos;
-  Vec vel;
-  float color[3];
-  struct timespec time;
-  struct Bullet *prev;
-  struct Bullet *next;
-  Bullet() {
-  prev = NULL;
-  next = NULL;
-  }
-  void operator=(Bullet x){
-  pos[0] = x.pos[0];
-  pos[1] = x.pos[1];
-  vel[0] = x.vel[0];
-  vel[1] = x.vel[1];
-  color[0] = x.color[0];
-  color[1] = x.color[1];
-  color[2] = x.color[2];
-  time = x.time;
-  }
-  };
-
-  const int MAX_BULLETS = 4;
-
-  struct Game {
-  Player Player_1;
-  Weapon gun;
-  Bullet *barr;
-  int nbullets;
-  int score = 0;
-  struct timespec bulletTimer;
-  Game() {
-  barr = new Bullet[MAX_BULLETS];
-  nbullets = 0;
-  }
-  };
-  */
-
-/*struct Person {
-  Vec dir;
-  Vec pos;
-  Vec lastpos;
-  Vec vel;
-  float angle;
-  float color[3];
-  Person() {
-  VecZero(dir);
-  pos[0] = (Flt)(xres/2);
-  pos[1] = (Flt)(yres/2);
-  pos[2] = 0.0f;
-  VecZero(vel);
-  angle = 0.0;
-  color[0] = 1.0;
-  color[1] = 1.0;
-  color[2] = 1.0;
-  }
-  };*/
 int keys[65536];
 int joy[65536];
 int axis[65536];
@@ -314,6 +255,7 @@ void init(Game *g)
 {
 		clock_gettime(CLOCK_REALTIME, &g->bulletTimer);
 		memset(keys, 0, 65536);
+		g->Player_1.stats.color[0] = 255;
 }
 
 void normalize(Vec v) 
