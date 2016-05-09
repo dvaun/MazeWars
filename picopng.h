380e3ed1a062b4715160c360715dfa1f4b86329c
David Vaughan <david@Davids-MacBook-Pro.local>
2016-05-08 22:47:43 -0700

    not sure what the issue is with the string not being found in std namespace, attempting to include multiple headers to temporarily fix issue

--- picopng.h
+++ picopng.h
@@ -2,6 +2,9 @@
 #define PICOPNG_H
 
 #include <vector>
+#include <stdlib.h>
+#include <sstream>
+#include <iomanip>
 #include <cstring>
 
 void loadFile(std::vector<unsigned char>& buffer, const std::string& filename);
