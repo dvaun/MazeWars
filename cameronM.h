279d34cb0c60facd1d58d40f632e79f580937e57
Cameron Morrow <cmorrow2@csub.edu>
2016-05-09 03:03:10 -0700

    Update cameronM.h

--- cameronM.h
+++ cameronM.h
@@ -1,6 +1,11 @@
 #ifndef CAMERON_H
 #define CAMERON_H
-
+void drawHealth(Player x);
+void drawAmmo(Player x);
+void drawDiamondBack(int x);
+//void drawArtifacts(Player x);
+void drawBackground();
+void drawHUD(Player x);
 struct HealthPack{
     Vec pos;
     int size;
