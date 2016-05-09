93ca9146f851d53feccde43539bc57eab62acfd2
David Vaughan <david@Davids-MacBook-Pro.local>
2016-05-08 22:45:25 -0700

    fixed picopng.h string issue

--- picopng.h
+++ picopng.h
@@ -2,8 +2,9 @@
 #define PICOPNG_H
 
 #include <vector>
+#include <stdlib.h>
 
-void loadFile(std::vector<unsigned char>& buffer, const std::string& filename);
+void loadFile(std::vector<unsigned char>& buffer, const string& filename);
 
 #endif
 /* PICOPNG_H */
