aedc735004e7bd8bfb8f4517c8472c5f8aaa4d56
rose <rphannavong@sleipnir.cs.csubak.edu>
2016-05-15 22:00:11 -0700

    sound changes

--- roseP.h
+++ roseP.h
@@ -1,8 +1,11 @@
 #ifndef ROSEP_H
 #define ROSEP_H
-
+#include </usr/include/AL/alut.h>
 // Name: Rose Phannavong
 
 void init_sounds();
+void release_sounds();
+void load_sounds();
+void play_sounds(int soundOption);
 
 #endif
