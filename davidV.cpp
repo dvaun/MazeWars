c6e75f07bbea7221ad130c40b9d54b58d49499a0
David Vaughan <dvaughan@csub.edu>
2016-06-01 03:05:57 -0700

    autoupdate

--- davidV.cpp
+++ davidV.cpp
@@ -716,14 +716,14 @@ void drawBlock(Game *g, gblock block)
 	glAlphaFunc(GL_GREATER, 0.0f);
 	glBindTexture(GL_TEXTURE_2D, g->blockTexture);
 	glBegin(GL_QUADS);
-		glTexCoord2f(cx2, cy1); glVertex2f(-size, -size);
+		/*glTexCoord2f(cx2, cy1); glVertex2f(-size, -size);
 		glTexCoord2f(cx2, cy2); glVertex2f(-size, size);
 		glTexCoord2f(cx1, cy2); glVertex2f(size, size);
-		glTexCoord2f(cx1, cy1); glVertex2f(size, -size);
-		/*glTexCoord2f(cx2, cy2); glVertex2f(-size, -size);
+		glTexCoord2f(cx1, cy1); glVertex2f(size, -size);*/
+		glTexCoord2f(cx2, cy2); glVertex2f(-size, -size);
 		glTexCoord2d(cx1, cy2); glVertex2f(-size, size);
 		glTexCoord2d(cx1, cy1); glVertex2f(size, size);
-		glTexCoord2f(cx2, cy1); glVertex2f(size, -size);*/
+		glTexCoord2f(cx2, cy1); glVertex2f(size, -size);
 	glEnd();
 	glDisable(GL_ALPHA_TEST);
 	glBindTexture(GL_TEXTURE_2D, 0);
